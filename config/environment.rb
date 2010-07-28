6209a91611b1de47a380aae1e0d8d5df49809e17
mperham <mperham@gmail.com>
2010-07-28 15:59:12 -0700

    Update to 2.3.8, add Hpricot gem required by text_helper.rb.

--- config/environment.rb
+++ config/environment.rb
@@ -7,7 +7,7 @@
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 #RAILS_GEM_VERSION = '2.1.2' unless defined? RAILS_GEM_VERSION
-RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -29,6 +29,7 @@ Rails::Initializer.run do |config|
   config.gem "ruby-openid", :lib => "openid"
   config.gem "aws-s3", :lib => "aws/s3"
   config.gem "jammit"
+  config.gem 'hpricot'
   
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
